6dc292eb3d938bc43b4a8331fa6b250ca5b8725b
mmorandini <emmemorandini@gmail.com>
2017-04-03 16:03:37 +0100

    added landing page and some styling

--- js/app.js
+++ js/app.js
@@ -1,139 +1,122 @@
 $(() => {
+
   var Game = Game || {};
-  let userMove = 0;
-  let expectedMove = 0;
-  let moveUp;
-  let moveDown;
-  let moveRight;
-  let moveLeft;
-  let scoreCounter = 0;
 
-  function movesTimer(){
-    window.setInterval(nextMove, 750);
-  }
+  $('#start-btn').on('click', function(){
+    console.log('clicked');
+    $(this).css('display','none');
+    $('.first').css('display','none');
+    $('h2').css('display','none');
+    $('header').css({
+      'paddingTop': '40px',
+      'height': '100px'
+    });
+    $('#game-wrapper').css({ display: 'block'});
 
-  function scoreCheck(){
-    if (expectedMove === userMove){
-      scoreCounter+=100;
-      console.log(scoreCounter);
-      $('#score').html(scoreCounter);
-      $('#arrow').animate({height: '140px', opacity: '1'}, '100');
-      $('#arrow').animate({height: '100px', opacity: '0.5'}, '100');
-    }else{
-      scoreCounter-=100;
-      console.log(scoreCounter);
-      $('#score').html(scoreCounter);
-      $('#arrow').animate({height: '140px', opacity: '1', backgroundColor: 'red'}, '100');
-      $('#arrow').animate({height: '100px', opacity: '0.5', backgroundColor: 'lime'}, '100');
+
+    let userMove = 0;
+    let expectedMove = 0;
+    let moveUp;
+    let moveDown;
+    let moveRight;
+    let moveLeft;
+    let scoreCounter = 0;
+
+
+    function movesTimer(){
+      window.setInterval(nextMove, 1000);
     }
-  }
 
-  //I want to check which one between moveUp or moveDown or moveLeft or moveRight is defined each interval.
-  function nextMove(){
-    const move = Math.random();///generates a random number for the computer to choose the next required move;
-    if (move < 0.25 ){
-      moveUp = move;
-      moveDown = undefined;
-      moveRight = undefined;
-      moveLeft = undefined;
-      expectedMove = 119;
-
-    }else if (move > 0.25 && move < 0.5){
-      moveDown = move;
-      moveUp = undefined;
-      moveRight = undefined;
-      moveLeft = undefined;
-      expectedMove = 115;
-
-    }else if (move > 0.5 && move < 0.75){
-      moveRight = move;
-      moveDown = undefined;
-      moveUp = undefined;
-      moveLeft = undefined;
-      expectedMove = 100;
-
-    }else{
-      moveLeft = move;
-      moveDown = undefined;
-      moveRight = undefined;
-      moveUp = undefined;
-      expectedMove = 97;
-
-    }
-    //Picks the right arrow image.
-    if (moveUp !== undefined){
-      console.log('YoUp!');
-      $('#arrow').attr('src','images/arrows/arrow-icon-up.png');
-    }else if(moveDown !== undefined){
-      console.log('YoDown!');
-      $('#arrow').attr('src','images/arrows/arrow-icon-down.png');
-    }else if (moveLeft !== undefined){
-      console.log('yoLeft!');
-      $('#arrow').attr('src','images/arrows/arrow-icon-left.png');
-    }else if (moveRight !== undefined){
-      console.log('yoRight!');
-      $('#arrow').attr('src','images/arrows/arrow-icon-right.png');
-    }
-  } // nextMove closing
-  //collect values of the keys pressed by the user and store them into an array
-  $(window).on('keypress', function(event) {
-    userMove = event.which;
-    console.log(event.which);
-    console.log(userMove);
-    console.log(expectedMove);
-    scoreCheck();
-
-    function dance() {
-      if (event.which === 119){
-        $('.dancer').attr('src','images/char/char5.png');
-      }else if(event.which === 115){
-        $('.dancer').attr('src','images/char/char2.png');
-      }else if(event.which === 100){
-        $('.dancer').attr('src','images/char/char3.png');
+    function scoreCheck(){
+      if (expectedMove === userMove){
+        scoreCounter+=100;
+        console.log(scoreCounter);
+        $('#score').html(scoreCounter);
+        $('#arrow').animate({opacity: '1'}, '100');
+        $('#arrow').animate({opacity: '0.5'}, '100');
       }else{
-        $('.dancer').attr('src','images/char/char4.png');
+        scoreCounter-=100;
+        console.log(scoreCounter);
+        $('#score').html(scoreCounter);
+        $('#arrow').animate({opacity: '1', backgroundColor: 'red'}, '100');
+        $('#arrow').animate({opacity: '0.5', backgroundColor: 'lime'}, '100');
       }
     }
-    dance();
-  });
-  //triggers the moves' timer
+    //I want to check which one between moveUp or moveDown or moveLeft or moveRight is defined each interval.
+    function nextMove(){
+      const move = Math.random();///generates a random number for the computer to choose the next required move;
+      if (move < 0.25 ){
+        moveUp = move;
+        moveDown = undefined;
+        moveRight = undefined;
+        moveLeft = undefined;
+        expectedMove = 38;
+      }else if (move > 0.25 && move < 0.5){
+        moveDown = move;
+        moveUp = undefined;
+        moveRight = undefined;
+        moveLeft = undefined;
+        expectedMove = 40;
+      }else if (move > 0.5 && move < 0.75){
+        moveRight = move;
+        moveDown = undefined;
+        moveUp = undefined;
+        moveLeft = undefined;
+        expectedMove = 39;
+      }else{
+        moveLeft = move;
+        moveDown = undefined;
+        moveRight = undefined;
+        moveUp = undefined;
+        expectedMove = 37;
+      }
 
-  movesTimer();
+      //Picks the right arrow image.
+      if (moveUp !== undefined){
+        // console.log('YoUp!');
+        $('#arrow').attr('src','images/arrows/arrow-icon-up.png');
+      }else if(moveDown !== undefined){
+        // console.log('YoDown!');
+        $('#arrow').attr('src','images/arrows/arrow-icon-down.png');
+      }else if (moveLeft !== undefined){
+        // console.log('yoLeft!');
+        $('#arrow').attr('src','images/arrows/arrow-icon-left.png');
+      }else if (moveRight !== undefined){
+        // console.log('yoRight!');
+        $('#arrow').attr('src','images/arrows/arrow-icon-right.png');
+      }
+
+    } // nextMove closing
+    //collect values of the keys pressed by the user and store them into an array
+    $(document).keydown(function(e) {
+      userMove = e.which;
+      scoreCheck();
+      switch(e.which) {
+        case 37: // left
+          $('.dancer').attr('src','images/char/TumpsDance11.png');
+          break;
+        case 38: // up
+          $('.dancer').attr('src','images/char/TumpsDance20.png');
+          break;
+        case 39: // right
+          $('.dancer').attr('src','images/char/TumpsDance10.png');
+          break;
+        case 40: // down
+          $('.dancer').attr('src','images/char/TumpsDance14.png');
+          break;
+        default: return; // exit this handler for other keys
+      }
+      setTimeout(()=>{
+        $('.dancer').attr('src','images/char/TumpsDance1.png');
+      }, 500);
+      e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+    //triggers the moves' timer
+    movesTimer();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });///end of click listener
 
 }); //doc ready closure
